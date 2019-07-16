751a065c3bea57febbc5e9af38d9453c88162501
vagnercs <vagnermasked@gmail.com>
2019-07-16 17:33:01 -0300

    Create 01B-maior-menor.c

---
+++ 2018/01-Estruturas-Condicionais/01B-maior-menor.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void)
+{
+    int a,b;
+    printf("insira dois valor:  ");
+    scanf("%d%d",&a,&b);
+    if(a > b){
+        printf("primeiro: %d\nsegundo: %d \n",a,b);
+    }else{
+        printf("primeiro: %d\nsegundo: %d \n",b,a);
+    }
+
+
+    system("PAUSE");
+    return 0;
+}
