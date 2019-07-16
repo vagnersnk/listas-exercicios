493b6e14a935891ade1811119fb5ef3ac24dac56
vagnercs <vagnermasked@gmail.com>
2019-07-16 15:33:16 -0300

    Create 01H-comprimento-esfera.c

---
+++ 2018/01-Algoritmos-Sequeciais-e-estrutura-condicionais/01H-comprimento-esfera.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+int main() {
+    float raio,resultado;
+    printf("Insira o raio: ");
+    scanf("%f",&raio);
+    resultado = 2*3.14*raio;
+    printf("O comprimento da esfera foi: %0.2f metros \n",resultado);
+    system("PAUSE");
+
+
+    system("PAUSE");
+    return 0;
+}
