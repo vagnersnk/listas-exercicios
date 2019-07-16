fb1395a9018157890e279909f094908122013535
vagnercs <vagnermasked@gmail.com>
2019-07-16 17:53:31 -0300

    add

---
+++ 2018/01-Estruturas-Condicionais/01E-soma-maior-com-menor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (void)
+{
+    int a,b,c;
+    printf("insira tres valor:  ");
+    scanf("%d%d%d",&a,&b,&c);
+    if (a > b) {
+        if (b > c) {
+            printf("%d \n",c+a);
+        }else if (a > c) {
+            printf("%dn", b + a);
+        }else {
+            printf("%d \n", b + c);
+        }
+    }else {
+        if (b > c) {
+            if (a > c) {
+                printf("%d \n", c + b);
+            }else {
+                printf("%d \n", a+ b);
+            }
+        }else {
+            printf("%d \n", a+ c);
+        }
+    }
+    system("PAUSE");
+    return 0;
+}
+
