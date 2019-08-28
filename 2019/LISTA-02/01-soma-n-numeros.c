448878be86b5113cb2bebd5ecd641943b30410a5
vagnercs <vagnermasked@gmail.com>
2019-08-28 16:57:58 -0300

    adicionar array

--- 2019/LISTA-02/01-soma-n-numeros.c
+++ 2019/LISTA-02/01-soma-n-numeros.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-int main() {
-	
-	int entrada,i=0,n=0;
-	printf("Insira um valor:");
-	scanf_s("%d", &entrada);
-	while (i < entrada) {
-		i = i + 1;
-		n = n + i;
-	}
-	printf_s("%d", n);
-	
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int entrada,limite,soma=0,i;
+    float media;
+    int lista[5];
+    printf("[MULTIPLOS] Digite o numero para iniciar: ");
+    scanf("%d",&entrada);
+    printf("Digite a quantidade de multiplos: ");
+    scanf("%d",&limite);
+    for(i=1;i<=limite;i++){
+        lista[i] = (entrada*i);
+        soma = soma + (entrada*i);
+
+        //printf("%d x %d = %d\n",entrada,i,entrada*i);
+        printf("[%d]",lista[i]);
+    }
+
+    //printf("Media da soma %d é %d\n",soma,soma/limite);
+
+
+
+
+
+
+
+
 	return 0;
 }
