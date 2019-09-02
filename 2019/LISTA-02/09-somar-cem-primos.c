e0496a66a594eb66c26680e24800760551e9f51e
vagnercs <vagnermasked@gmail.com>
2019-09-02 17:50:41 -0300

    arrumar primos

--- 2019/LISTA-02/09-somar-cem-primos.c
+++ 2019/LISTA-02/09-somar-cem-primos.c
@@ -2,34 +2,22 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main()
-{
-    setlocale(LC_ALL, "Portuguese");
-    int inicio = 2,div=0,count,soma=0;
+int main() {
+    int cont = 0, i, j;
+    int soma = 0;
 
-    while(inicio < 100)
-    {
-        for(div=1; div<=100; div++)
-        {
-            if(inicio%div == 0)
-                count++;
+    for (i=1 ; i <= 100; i++) {
 
+        for (j=1; j <= i; j++) {
+            if (i % j == 0) {
+                cont++;
+            }
         }
-
-        if(count==2)
-        {
-
-            soma = soma+inicio;
-
+        if (cont == 2) {
+            printf("%d\n", i);
+            soma= soma+i;
         }
-
-        count = 0;
-        inicio++;
-
-    }
-    printf("%i\n",soma);
-
-    
-	
-	return 0;
+        cont = 0;
+    } printf("Soma dos 100 primeiros numero primos = %d\n", soma);
+    return 0;
 }
