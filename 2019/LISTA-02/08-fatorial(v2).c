0d0158d463fcf3f43661de3c0d0d7703bc11f46d
vagnercs <vagnermasked@gmail.com>
2019-09-08 15:00:38 -0300

    fatorial v2

---
+++ 2019/LISTA-02/08-fatorial(v2).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int entrada,i,fat=1;
+
+    printf("Digite Valores [Digite(-1) para sair]");
+    while(1){
+        scanf("%d",&entrada);
+        if (entrada != -1){
+            for(i=1;i<=entrada;entrada--){
+                fat = fat *entrada;
+            }
+            printf("%i\n",fat);
+            fat = 1;
+
+
+        }else{
+            printf("Programa encerrado");
+            break;
+        }
+
+
+    }
+    return 0;
+}
