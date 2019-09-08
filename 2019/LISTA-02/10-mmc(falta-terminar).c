ee584ece5277c32437d4b78f22ec5cca4e631f6e
vagnercs <vagnermasked@gmail.com>
2019-09-08 13:18:40 -0300

    arrumar mmc

--- 2019/LISTA-02/10-mmc(falta-terminar).c
+++ 2019/LISTA-02/10-mmc(falta-terminar).c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 int main() {
-   int num1,num2,i,j,div=0,div2=0;
+   int num1,num2,maior=0;
    printf("Insira dois numeros: ");
    scanf("%i%i",&num1,&num2);
-   if(num1>num2){
-       for(i=0;i<num1;i++){
-           div = num1*i;
-           //printf("1[%i]\n",div);
-          for(j=0;j<num1;j++){
-               //printf("2[%i]\n",div2);
-               div2= num2*j;
-
+   if (num1>num2){
+       maior = num1;
+   }else{
+       maior = num2;
+   }
+   while(1){
+       if (maior % num1 ==0 && maior % num2 ==0){
+           printf("%i",maior);
+           break;
+       }else{
+           maior++;
        }
-
-
-       }if(div = div2){
-           printf("I[%i]\n",div);
-           //break;
-       }
-
-
    }
 
     return 0;
