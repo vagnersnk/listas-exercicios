b37088963fc6fb0971a3ea10af8a4ac6a67dfe55
vagnercs <vagnermasked@gmail.com>
2019-09-17 17:59:29 -0300

    add ex8

---
+++ 2019/LISTA-03/08-ler-maior-menor-e-indice.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define LIM 4
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int vetor[LIM], i,maior,menor,indice1=0,indice2=0;
+    printf("Digite %i valores",LIM);
+
+    for (i = 0; i < LIM; i++){
+        scanf("%d",&vetor[i]);
+
+    }
+    maior= vetor[0];
+    menor= vetor[0];
+    for (i = 1; i < LIM; i++){
+       if(vetor[i]>maior){
+            maior = vetor[i];
+            indice1 = i;
+        }
+        if(vetor[i]<menor){
+            maior = vetor[i];
+            indice2 = i;
+        }
+    }
+    printf("O maior valor [%i] se encontra no indice [%d]\no menor valor  [%i] se encontra no indice [%d]",maior,indice1+1,menor,indice2+1);
+
+
+
+    return 0;
+}
+
