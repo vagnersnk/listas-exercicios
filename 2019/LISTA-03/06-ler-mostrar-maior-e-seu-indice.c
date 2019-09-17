22f5be431886a5d9008e53d9a4161085dec5de39
vagnercs <vagnermasked@gmail.com>
2019-09-17 17:49:25 -0300

    add ex6

---
+++ 2019/LISTA-03/06-ler-mostrar-maior-e-seu-indice.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define LIM 4
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int vetor[LIM], i,maior,indice=0;
+    printf("Digite %i valores",LIM);
+
+    for (i = 0; i < LIM; i++){
+        scanf("%d",&vetor[i]);
+
+    }
+    maior= vetor[0];
+    for (i = 1; i < LIM; i++){
+       if(vetor[i]>maior){
+           maior = vetor[i];
+           indice = i;
+       }
+    }
+    printf("O maior valor [%i] se encontra no indice [%d]",maior,indice+1);
+
+
+
+    return 0;
+}
