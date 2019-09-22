6ca20ce50934b99369ba5e9faf8d908421fc340e
vagnercs <vagnermasked@gmail.com>
2019-09-21 22:40:38 -0300

    add ex 19

---
+++ 2019/LISTA-03/19-ordenar-vetor-ciquenta.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define REAGENTE_LIMITANTE 50
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int vetor_iti[REAGENTE_LIMITANTE],i,j,t;
+    printf("LISTA 1\n");
+    for (i = 0; i < REAGENTE_LIMITANTE; i++){
+        printf("[%d] = ",i);
+        scanf("%d",&vetor_iti[i]);
+
+    }
+
+
+    for (i = 0; i < REAGENTE_LIMITANTE; i++) {
+        for(j=i+1;j<REAGENTE_LIMITANTE;j++){
+            t = vetor_iti[i];
+            vetor_iti[i] = vetor_iti[j];
+            vetor_iti[j] = t;
+
+        }
+
+
+    }
+    for (i=0;i<REAGENTE_LIMITANTE;i++) {
+        printf("[%d]\t",vetor_iti[i]);
+
+
+    }
+
+    return 0;
+}
