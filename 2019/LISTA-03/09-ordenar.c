f45971033f676db1366df37d219d708fa1db427d
vagnercs <vagnermasked@gmail.com>
2019-10-03 14:39:20 -0300

    ex9

--- 2019/LISTA-03/09-ordenar.c
+++ 2019/LISTA-03/09-ordenar.c
@@ -1,51 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
-#define LIM 4
-
-//EXIBIR INTENS DA LISTA
-//printf("(INDICE)-VALOR\n=-=-=-=-=-=-=\n");
-//for(i=0;i<LIM;i++)
-//  printf("(%d) - [%d]\n",i,lista[i]);
-
+#define REAGENTE_LIMITANTE 3
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
-    int lista[LIM],i,j,t,maior,imaior;
+   int lista[REAGENTE_LIMITANTE],i,j,t,maior,indice;
+   printf("DIGITE %d VALORES\n",REAGENTE_LIMITANTE);
+   for(i=0;i<REAGENTE_LIMITANTE;i++){
+       printf(">> [%d]",i);
+       scanf("%d",&lista[i]);
+   }
+   for(i=0;i<REAGENTE_LIMITANTE;i++){
+       maior = lista[i];
+       indice=i;
+       for(j=i+1;j<REAGENTE_LIMITANTE;j++){
+           if(lista[i] > maior){
+               maior = lista[i];
+               indice = i;
+           }
+       }
+       t = lista[i];
+       lista[j] = maior;
+       lista[indice] = t;
 
-    //LER DADOS
-    printf("Insira %d valores:\n",LIM);
-    for(i=0;i<LIM;i++)
-      scanf("%d",&lista[i]);
 
-    //-=-=-=-=-=RELATORIO-=-=-=-=-=-=
-    // ENTRADA PREVISTA [3,2,4,6]
-    // RESULTADO ESPERADO DA ENTRADA [6,4,3,2]
-    // RESULTADO ESPERADO DA LISTA DE INDICE [3,2,0,1]
+   }
 
-    //ACHAR PRIMEIRO INDICE
-    for(j=0;j<LIM-1;j++){
-      maior=lista[j];
-      imaior = j;
-      for(i=j+1;i<LIM;i++){      
-         if(lista[i]>maior){
-           maior = lista[i];
-           imaior = i;
-          }
-      t = lista[j];
-      lista[j] = maior;
-      lista[imaior] = t;
-      }
 
-    }
-
-    //EXIBIR INTENS DA LISTA
-    printf("EXIBINDO LISTA DE INDICE\n");
-    for(i=0;i<LIM;i++)
-      printf("(%d) - [%d]\n",i,lista[i]);
-
+   for(i=0;i<REAGENTE_LIMITANTE;i++)
+       printf("[%d]",lista[i]);
 
 
 
