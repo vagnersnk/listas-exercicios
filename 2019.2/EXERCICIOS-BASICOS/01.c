19223f3093a2c7f85058b00429914844d94993d0
vagnercs <vagnermasked@gmail.com>
2019-11-30 13:35:31 -0300

    adicionar ex 1

---
+++ 2019.2/EXERCICIOS-BASICOS/01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+#define MAX 5
+
+
+
+int main() {
+    int lista[MAX],i,ma;
+    for(i=0;i<MAX;i++){
+        printf("[%d]:",i);
+        scanf("%d",&lista[i]);
+    }
+    ma = lista[0];
+    for(i=1;i<MAX;i++){
+        if(lista[i]> ma){
+            ma = lista[i];
+        }
+
+
+    }
+    printf("Maior idade: %d",ma);
+
+
+    return 0;
+}
