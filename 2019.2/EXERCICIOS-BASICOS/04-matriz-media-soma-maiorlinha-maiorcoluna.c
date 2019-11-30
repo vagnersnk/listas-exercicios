67db2fce10c6c40223c6ded6aae625c9987c610f
vagnercs <vagnermasked@gmail.com>
2019-11-30 14:37:38 -0300

    add ex 4 (arrumar colunas)

---
+++ 2019.2/EXERCICIOS-BASICOS/04-matriz-media-soma-maiorlinha-maiorcoluna.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+
+#define LIN 2
+#define COL 4
+
+
+
+int main() {
+    int lista[LIN][COL],i,j,maiorl,soma=0,k=0;
+
+    for(i=0;i<LIN;i++){
+        for(j=0;j<COL;j++){
+            printf("[%d][%d]:",i,j);
+            scanf("%d",&lista[i][j]);
+
+        }
+    }
+    for(i=0;i<LIN;i++){
+        for(j=0;j<COL;j++){
+            printf("[%d]\t",lista[i][j]);
+
+        }
+        printf("\n");
+    }
+
+
+    for(i=0;i<LIN;i++){
+        maiorl = lista[0][0];
+        
+        for(j=0;j<COL;j++){
+            soma += lista[i][j];
+
+           if(lista[i][j] > maiorl){
+               maiorl = lista[i][j];
+           }
+
+        }
+
+        printf("maior da %d linha foi: %d\n",i,maiorl);
+
+    }
+    printf("soma da matriz = %d\n",soma);
+    printf("media da matriz = %d\n",soma/LIN*COL);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
