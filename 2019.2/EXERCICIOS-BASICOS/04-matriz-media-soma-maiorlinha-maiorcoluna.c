73fde92dde8499914ce30709fb65e30b88af5e26
vagnercs <vagnermasked@gmail.com>
2019-11-30 14:40:56 -0300

    fix ex 4

--- 2019.2/EXERCICIOS-BASICOS/04-matriz-media-soma-maiorlinha-maiorcoluna.c
+++ 2019.2/EXERCICIOS-BASICOS/04-matriz-media-soma-maiorlinha-maiorcoluna.c
@@ -16,13 +16,7 @@ int main() {
 
         }
     }
-    for(i=0;i<LIN;i++){
-        for(j=0;j<COL;j++){
-            printf("[%d]\t",lista[i][j]);
 
-        }
-        printf("\n");
-    }
 
 
     for(i=0;i<LIN;i++){
