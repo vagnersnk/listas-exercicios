d51409da6fd49674c74c9bad602ea4b6e5212077
vagnercs <vagnermasked@gmail.com>
2020-01-02 09:54:19 -0300

    add ex 1

--- 2019.2/LISTA-02/01-inversa-string.c
+++ 2019.2/LISTA-02/01-inversa-string.c
@@ -1,1 +1,16 @@
-int main(){return 0;}
+#include <stdio.h>
+#include <string.h>
+
+#define LIM 10
+
+int main() {
+    char string[LIM];
+    int i;
+    puts("Insira a string");
+    gets(string);
+    for(i=strlen(string)-1;i>-1;i--){
+        printf("%c",string[i]);
+    }
+
+    return 0;
+}
