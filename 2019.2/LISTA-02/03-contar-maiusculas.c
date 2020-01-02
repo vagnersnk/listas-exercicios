c1d4384c8b65a71ff9975f9886879af1c1643967
vagnercs <vagnermasked@gmail.com>
2020-01-02 10:19:46 -0300

    adicionar exercicio 3 -contar maisculas

---
+++ 2019.2/LISTA-02/03-contar-maiusculas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define LIM 10
+
+int main() {
+    char string[LIM];
+    int i,count;
+    puts("Insira a string");
+    gets(string);
+    count = 0;
+    for(i=0;i<strlen(string)-1;i++){
+        if(isupper(string[i])){
+            count++;
+        }
+    }
+    printf("palavra com: %d maisculas",count);
+
+
+
+
+    return 0;
+}
