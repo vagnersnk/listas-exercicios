9b9726c0f874a07c19e9fafc8238824f50c1832b
vagnercs <vagnermasked@gmail.com>
2020-01-05 19:19:43 -0300

    arrumar intercalar

--- 2019.2/LISTA-02/05-intercalar-strings.c
+++ 2019.2/LISTA-02/05-intercalar-strings.c
@@ -5,8 +5,8 @@
 #define LIM 10
 
 int main() {
-    char stringone[LIM],stringtwo[LIM],stringtre[LIM];
-    int i,j,k;
+    char stringone[LIM],stringtwo[LIM];
+    int i,j;
     puts("Insira a primeira string");
     gets(stringone);
     puts("Insira a segunda string");
@@ -14,18 +14,14 @@ int main() {
     int tamanho1 = strlen(stringone);
     int tamanho2 = strlen(stringtwo);
     int tamanhototal = tamanho1+tamanho2;
-    for(i=0;i<tamanhototal;i++){
-        for(j=0;j<tamanho1;j++){
-            for(k=0;k<tamanho2;k++){
-                stringtre[i] = stringone[j] + stringtwo[k];
-            }
-        }
-
-
+    char stringtre[tamanhototal];
+    for(i=0,j=0;i<tamanhototal;i+=2,j++){
+        
+                stringtre[i] = stringone[j];
+                stringtre[i+1] = stringtwo[j];
     }
-
     puts(stringtre);
-    
+
 
     return 0;
 }
